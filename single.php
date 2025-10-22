08858c2be82e7318fef83d64370f49f88aca4be1
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2025-10-22 11:26:56 -0300

    feat: add page single post

--- single.php
+++ single.php
@@ -5,31 +5,33 @@ get_header();
 
 <main id="pg-single" role="main">
   <section class="section-intro">
-    <?php
-    if (have_posts()):
-      while (have_posts()):
-        the_post();
-        ?>
+    <div class="container">
+      <?php
+      if (have_posts()):
+        while (have_posts()):
+          the_post();
+          ?>
 
-        <div class="blog-card-top">
-          <?php
-          $categories = get_the_category();
-          if ($categories): ?>
-            <span class="blog-card-category"><?php echo esc_html($categories[0]->name); ?></span>
-          <?php endif; ?>
+          <div class="blog-card-top">
+            <?php
+            $categories = get_the_category();
+            if ($categories): ?>
+              <span class="blog-card-category"><?php echo esc_html($categories[0]->name); ?></span>
+            <?php endif; ?>
 
-          <?php if (get_field('data_do_post')): ?>
-            <span class="blog-card-date"><?php the_field('data_do_post'); ?></span>
+            <?php if (get_field('data_do_post')): ?>
+              <span class="blog-card-date"><?php the_field('data_do_post'); ?></span>
+            <?php endif; ?>
+          </div>
+
+          <h1 class="title-section"><?php the_title(); ?></h1>
+
+          <?php if (has_post_thumbnail()): ?>
+            <div class="single-post-img">
+              <?php the_post_thumbnail('large'); ?>
+            </div>
           <?php endif; ?>
         </div>
-
-        <h1 class="single-post-title"><?php the_title(); ?></h1>
-
-        <?php if (has_post_thumbnail()): ?>
-          <div class="single-post-img">
-            <?php the_post_thumbnail('large'); ?>
-          </div>
-        <?php endif; ?>
       </section>
 
       <section class="section-content">
@@ -40,17 +42,18 @@ get_header();
 
       <section class="section-blog-navigation">
         <a href="/blog">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/icons/blog.svg" alt="icone arrow">
           <span>volte ao blog</span>
         </a>
       </section>
 
       <?php
-      endwhile;
-    else:
-      ?>
+        endwhile;
+      else:
+        ?>
     <p>Post não encontrado.</p>
     <?php
-    endif;
-    ?>
-</main><?php get_footer(); ?>
+      endif;
+      ?>
+</main>
+
+<?php get_footer(); ?>
